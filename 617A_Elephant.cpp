ac9ba874f8b122514c86f46608847bc487cc8933
Rishika <rishikakashyap2552@gmail.com>
2026-03-07 00:26:16 +0530

    Create 617A_Elephant.cpp

---
+++ 617A_Elephant.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cin >> x;
+  // Ceiling division: (x + divisor - 1) / divisor
+    int moves = (x + 4) / 5;
+    cout << moves << endl;
+}
